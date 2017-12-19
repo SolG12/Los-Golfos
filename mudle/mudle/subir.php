24ee33b837ea6f5601b1976a47a15c33fc08ff62
Batres35 <31639031+Batres35@users.noreply.github.com>
2017-12-18 18:50:22 -0600

    Texto en Linea
    Se agrego la opcion de agregar texto en linea al enviar una tarea.

---
+++ mudle/mudle/subir.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Enviar Tarea</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="//fonts.googleapis.com/css?family=Cormorant+Garamond:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Arsenal:400,400i,700,700i" rel="stylesheet">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<ul>
+  		<li><a href="productos.php">Productos</a></li>
+  		<li><a href="alta_producto.php">Dar de Alta Producto</a></li>
+ 		<li><a href="login.php">Login</a></li>
+ 		<li><a href="logout.php">Logout</a></li>
+	</ul>
+	<div class="main-agileinfo slider ">
+		<div class="items-group">
+			<div class="item agileits-w3layouts">
+				<div class="block text main-agileits"> 
+					<span class="circleLight"></span> 
+					<!-- login form -->
+					<div class="login-form loginw3-agile"> 
+						<div class="agile-row">
+							<h1>Enviar Tarea</h1> 
+							<div class="login-agileits-top">
+							<?php
+							$id_producto = $_GET['id_producto'];
+							 ?>
+							<form action="cargar.php" method="post" enctype="multipart/form-data"> 	
+								<input type="hidden" name="id_producto" value=<?php echo $id_producto?>>
+								<p>Numero de Control</p>
+								<input type="text" name="nc" maxlength="10" class="name" required>
+								<p>Texto</p>
+								<textarea class="form-control" rows="5" name="texto"></textarea>
+								<p>Archivo</p>
+								<input type="file" name="archivo" value="Seleccionar Archivo">
+								<br>
+								<br>
+								<input type="submit" name="Submit" value="Enviar">
+							</form> 
+							</div> 
+							<!--
+							<div class="login-agileits-bottom wthree"> 
+								<h6><a href="#">Forgot password?</a></h6>
+							</div> 
+							--> 
+						</div>  
+					</div>   
+				</div>
+				<div class="footeragileits">
+					<p> &copy; 2017 Derechos Reservados | Los Golfos</a></p>
+				</div> 
+			</div>   
+		</div>
+	</div>	 
+	<!-- //main --> 
+</body>
+</html>
---
+++ mudle/mudle/ver_texto.php
@@ -0,0 +1,93 @@
+<?php
+	session_start();
+
+	if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+	} else {
+   		echo "Necesitas acceder para registrar un administrador.<br>";
+   		echo "<br><a href='login.php'>Login</a>";
+		exit;
+	}
+
+	$now = time();
+
+	if($now > $_SESSION['expire']) {
+		session_destroy();
+		echo "Su sesion a terminado,
+		<a href='login.php'>Necesita Hacer Login</a>";
+		exit;
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Productos</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="//fonts.googleapis.com/css?family=Cormorant+Garamond:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Arsenal:400,400i,700,700i" rel="stylesheet">
+</head>
+<body> 
+	<ul>
+  		<li><a href="productos.php">Productos</a></li>
+  		<li><a href="alta_producto.php">Dar de Alta Producto</a></li>
+ 		<li><a href="login.php">Login</a></li>
+ 		<li><a href="logout.php">Logout</a></li>
+	</ul>
+	<div class="main-agileinfo slider ">
+		<div class="items-group">
+			<div class="item agileits-w3layouts">
+				<div class="block text main-agileits"> 
+					<span class="circleLight"></span> 
+					<!-- login form -->
+					<div class="login-form loginw3-agile"> 
+						<div class="agile-row">
+							<h1>Ver Producto</h1> 
+							<div class="login-agileits-top">
+
+<?php
+
+$host_db = "localhost";
+$user_db = "root";
+$pass_db = "";
+$db_name = "mudle";
+$tbl_name = "carga";
+
+$conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
+
+if ($conexion->connect_error) {
+ die("La conexion falló: " . $conexion->connect_error);
+}
+ 
+$sql = "SELECT texto FROM carga WHERE id_carga = $_GET[id_carga]";
+
+$result = $conexion->query($sql);
+
+if ($result->num_rows > 0) {
+	$row = $result->fetch_array(MYSQLI_ASSOC);
+ }
+
+ echo $row['texto'];
+
+ 
+ mysqli_close($conexion); 
+ ?>
+							</div> 
+							<!--
+							<div class="login-agileits-bottom wthree"> 
+								<h6><a href="#">Forgot password?</a></h6>
+							</div> 
+							--> 
+						</div>  
+					</div>   
+				</div>
+				<div class="footeragileits">
+					<p> &copy; 2017 Derechos Reservados | Los Golfos</a></p>
+				</div> 
+			</div>   
+		</div>
+	</div>	 
+	<!-- //main --> 
+</body>
+</html>
